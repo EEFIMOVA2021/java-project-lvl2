2c2a4bb41a8659b552d08df96a69f03a6197a80f
EEFIMOVA <ekaterinaefimova2015@mail.ru>
2022-03-20 20:56:45 +0300

    step5 part 7

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -49,12 +49,12 @@ public class App implements Runnable{
             String json1 = getJsonFromFile(filepath1);
             String json2 = getJsonFromFile(filepath2);
             ObjectMapper objectMapper = new ObjectMapper();
-            Map<String, String> map1 = objectMapper.readValue(json1, Map.class);
-            Map<String, String> map2 = objectMapper.readValue(json2, Map.class);
+            Map<String, Object> map1 = objectMapper.readValue(json1, Map.class);
+            Map<String, Object> map2 = objectMapper.readValue(json2, Map.class);
             System.out.println(map1);
             System.out.println(map2);
             Map<String, String> res = genDiff(map1, map2);
-            System.out.println(res);
+            System.out.println(getStringValuesMap(res));
         } catch (Exception e) {
             System.out.println("An error has ossured in main()!");
             e.printStackTrace();
@@ -74,21 +74,30 @@ public class App implements Runnable{
         return json;
     }
 
-    private static Map<String, String> genDiff(Map<String, String> map1, Map<String, String> map2) {
+    private static Map<String, String> genDiff(Map<String, Object> map1, Map<String, Object> map2) {
         Map<String, String> result = new LinkedHashMap<>();
         Set<String> keys = new TreeSet<>(map1.keySet());
         keys.addAll(map2.keySet());
         for (String key: keys) {
             if(!map1.containsKey(key)) {
-                result.put(key, "added");
+                result.put(key, "+ " + key + ": " + map2.get(key));
             } else if (!map2.containsKey(key)) {
-                result.put(key, "deleted");
-            } /*else if (map1.get(key).equals(map2.get(key))) {
-                result.put(key, "unchanged");
+                result.put(key, "- " + key + ": " + map1.get(key));
+            } else if (map1.get(key).equals(map2.get(key))) {
+                result.put(key, "  " + key + ": " + map1.get(key));
             } else {
-                result.put(key, "changed");
-            }*/
+                result.put(key, "- " + key + ": " + map1.get(key) + "\n+ " + key + ": " + map2.get(key));
+            }
         }
+        return result;
+    }
+
+    private static String getStringValuesMap (Map<String, String> map) {
+        String result = "{";
+        for(Map.Entry<String, String> mapEntity: map.entrySet()) {
+            result += "\n" + mapEntity.getValue();
+        }
+        result += "\n}";
         return result;
     }
 }
